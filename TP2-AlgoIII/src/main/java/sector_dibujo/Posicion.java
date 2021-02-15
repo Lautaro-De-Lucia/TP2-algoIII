f1b752c780ade4095e9e8fa35c4899f8c44149a5
lautaro <ldelucia@fi.uba.ar>
2021-02-15 18:05:00 -0300

    Agregué métodos y atributos a la clase posición

--- TP2-AlgoIII/src/main/java/sector_dibujo/Posicion.java
+++ TP2-AlgoIII/src/main/java/sector_dibujo/Posicion.java
@@ -1,5 +1,23 @@
 package sector_dibujo;
 
 public class Posicion {
+	
+	int coordX;
+	int coordY;
+	
+	public Posicion() {
+		coordX = 0;
+		coordY = 0;
+	}
+	
+	public Posicion (int X, int Y) {
+		coordX = X;
+		coordY = Y;
+	}
+	
+	public void cambiar(Direccion direccion) {
+		coordX = coordX + direccion.longitud();
+		coordY = coordY + direccion.latitud();
+	}
 
 }
