495e6e0c5fca4ae6c8e94529ec4d9a4306074146
esp-ram <resperon@fi.uba.ar>
2021-02-23 17:25:33 -0300

    Clase linea y SectorDibujo con tests

--- TP2-AlgoIII/src/main/java/sector_dibujo/Linea.java
+++ TP2-AlgoIII/src/main/java/sector_dibujo/Linea.java
@@ -1,5 +1,38 @@
 package sector_dibujo;
 
 public class Linea {
+    Posicion origen;
+    Posicion destino;
+    // Color color;
 
+
+    public Linea(Posicion nuevoOrigen, Posicion nuevoDestino){
+        this.origen = nuevoOrigen;
+        this.destino = nuevoDestino;
+    }
+
+    public void setDestino(Posicion destino) {
+        this.destino = destino;
+    }
+
+    public Posicion getOrigen() {
+        return origen;
+    }
+
+    public Posicion getDestino() {
+        return destino;
+    }
+
+    public void setOrigen(Posicion origen) {
+        this.origen = origen;
+    }
+
+    public boolean igualA(Linea otraLinea){
+        if(origen == otraLinea.getOrigen()){
+            if(destino == otraLinea.getDestino()){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- TP2-AlgoIII/src/main/java/sector_dibujo/SectorDibujo.java
+++ TP2-AlgoIII/src/main/java/sector_dibujo/SectorDibujo.java
@@ -1,5 +1,45 @@
 package sector_dibujo;
+import java.util.LinkedList;
+
 
 public class SectorDibujo {
+    private Personaje personaje;
+    private LinkedList<Linea> tablero;
 
+    public SectorDibujo( ){
+        this.personaje = new Personaje();
+        this.tablero = new LinkedList<Linea>();
+    }
+
+    public void abajo(){
+        this.tablero.addLast(personaje.mover(new sur()));
+    }
+
+    public void arriba(){
+        this.tablero.addLast(personaje.mover(new norte()));
+    }
+
+    public void derecha(){
+        this.tablero.addLast(personaje.mover(new este()));
+    }
+
+    public void izquierda(){
+        this.tablero.addLast(personaje.mover(new oeste()));
+    }
+
+    public void bajarLapiz(){
+        this.personaje.bajarLapiz();
+    }
+
+    public void levantarLapiz(){
+        this.personaje.levantarLapiz();
+    }
+
+    public void ponerBorrador(){
+        this.personaje.ponerLapizEnBorrador();
+    }
+
+    public Linea ObtenerLinea(){
+        return this.tablero.getFirst();
+    }
 }
