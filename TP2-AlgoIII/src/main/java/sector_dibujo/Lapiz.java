291f8acae51905803db2ad4135e31e5eee89d62f
aguss <apuglisi@fi.uba.ar>
2021-02-23 15:22:12 -0300

    Agregado el código de lápiz, lápizbajo, lápizlevantado y sus pruebas

--- TP2-AlgoIII/src/main/java/sector_dibujo/Lapiz.java
+++ TP2-AlgoIII/src/main/java/sector_dibujo/Lapiz.java
@@ -2,4 +2,8 @@ package sector_dibujo;
 
 public interface Lapiz {
 
+    public Linea dibujarLinea(Posicion posicion1, Posicion posicion2){
+        return dibujarLinea(posicion1, posicion2);
+    }
+
 }
--- TP2-AlgoIII/src/main/java/sector_dibujo/LapizBajo.java
+++ TP2-AlgoIII/src/main/java/sector_dibujo/LapizBajo.java
@@ -1,5 +1,14 @@
 package sector_dibujo;
 
-public class LapizBajo {
+public class LapizBajo implements sector_dibujo.Lapiz {
+
+    public Linea dibujarLinea(Posicion posicion1, Posicion posicion2){
+
+        coordenadaInicial = new Posicion(posicion1);
+        coordenadaFinal = new Posicion(posicion2);
+
+        return (new Linea(coordenadaInicial, coordenadaFinal));
+
+    }
 
 }
