97e3e81434fd0ad30ac7eb4ce9a90325f0ef570d
mcollazo <mcollazo@mcollazo-desktop>
2021-02-23 16:26:26 -0300

    codificacion de clases Personaje, Posicion y Direccion, junto a tests para Personaje y Posicion

--- TP2-AlgoIII/src/main/java/sector_dibujo/Direccion.java
+++ TP2-AlgoIII/src/main/java/sector_dibujo/Direccion.java
@@ -3,27 +3,43 @@ package sector_dibujo;
 public class Direccion {
 	
 	private int latitud;
-	
 	private int longitud;
 	
-	private Direccion(int newLat, int newLong) {
+	public Direccion() {
+        this.latitud = 0;
+        this.longitud = 0;
+    }
+	
+	public Direccion(int newLat, int newLong) {
         this.latitud = newLat;
         this.longitud = newLong;
     }
 	
+	public int obtenerLatitud() {
+		return this.latitud;
+	}
+	
+	public int obtenerLongitud() {
+		return this.longitud;
+	}
+	
 	public void norte() {
-		
+		this.latitud = 0;
+        this.longitud = 1;
 	}
 	
 	public void sur() {
-		
+		this.latitud = 0;
+        this.longitud = -1;
 	}
 
 	public void este() {
-	
-}
+		this.latitud = 1;
+        this.longitud = 0;	
+	}
 
 	public void oeste() {
-	
-}
+		this.latitud = -1;
+        this.longitud = 0;	
+	}
 }
--- TP2-AlgoIII/src/main/java/sector_dibujo/Personaje.java
+++ TP2-AlgoIII/src/main/java/sector_dibujo/Personaje.java
@@ -1,5 +1,56 @@
 package sector_dibujo;
 
 public class Personaje {
-
+	
+	private Direccion orientacion;
+	private Posicion posicion;
+	private Lapiz lapiz;
+		
+	public Personaje() {
+		this.orientacion = new Direccion();
+		this.orientacion.sur();
+		this.posicion = new Posicion(0, 0);
+		this.lapiz = new LapizBajo();
+	}
+	
+	public Personaje(Direccion orientacion, Posicion posicion, Lapiz lapiz) {
+		this.orientacion = orientacion;
+		this.posicion = posicion;
+		this.lapiz = lapiz;
+	}
+	
+	public void levantarLapiz() {
+	 
+		this.lapiz = new LapizLevantado();
+	}
+	
+	public void bajarLapiz() {
+		 
+		this.lapiz = new LapizBajo();
+	}
+	
+	public void ponerLapizEnBorrador() {
+		 
+		this.lapiz = new LapizEnBorrador();
+	}
+	
+	public void nuevoColorDeLapiz(Color colorNuevo) {
+		 
+		this.lapiz.cambiarColor(colorNuevo);
+	}
+	
+	public Linea mover(Direccion direccionNueva) {
+		
+		Posicion posicionOriginal = new Posicion(this.posicion);
+		Posicion nuevaPosicion = new Posicion(this.posicion, direccionNueva);
+		
+		
+        this.posicion = nuevaPosicion;
+        this.orientacion = direccionNueva;
+        
+        Linea nuevaLinea = this.lapiz.dibujarLinea(posicionOriginal, nuevaPosicion);
+        
+        return nuevaLinea; 
+	}
+		
 }
